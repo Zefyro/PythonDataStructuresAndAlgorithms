f1c9013c2fb73ab870da125a40cc0b7e6cb0a984
Lenni Siggberg <lenni.siggberg@gmail.com>
2025-10-07 15:26:19 +0300

    Added explicit types and descriptions to stack.py

--- src/stack.py
+++ src/stack.py
@@ -1,26 +1,47 @@
+from typing import Any
 from .container import ContainerInterface
 
 class Stack(ContainerInterface):
-    def __init__(self):
-        self.items = []
+    """
+    An implementation of a stack.  
+    A stack follows a "last-in, first-out" (LIFO) principle.
+    """
+    def __init__(self) -> None:
+        self.items: list[Any] = []
     
-    def is_empty(self):
+    def is_empty(self) -> bool:
+        """
+        Returns: True if the stack is empty, false otherwise.
+        """
         return not self.items
     
-    def push(self, item):
+    def push(self, item) -> None:
+        """
+        Adds the item to the top of the stack.
+        """
         self.items.append(item)
     
-    def pop(self):
+    def pop(self) -> Any | None:
+        """
+        Removes the item at the top of the stack.  
+        Returns: The item at the top of the stack, or None if the stack is empty.
+        """
         if not self.is_empty():
             return self.items.pop()
         else:
-            return self.items
+            return None
     
-    def peek(self):
+    def peek(self) -> Any | None:
+        """
+        Returns: The item at the top of the queue, or None if the stack is empty.
+        """
         if not self.is_empty():
             return self.items[-1]
         else:
             return None
     
-    def size(self):
+    def size(self) -> int:
+        """
+        Returns: The size of the stack.
+        """
         return len(self.items)
