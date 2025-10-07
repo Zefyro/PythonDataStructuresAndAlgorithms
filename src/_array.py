e424fddba2f1c3ab7a00b0107dcce63ff1359b33
Lenni Siggberg <lenni.siggberg@gmail.com>
2025-10-07 17:38:08 +0300

    An implementation of an array, and tests

---
+++ src/_array.py
@@ -0,0 +1,30 @@
+from typing import Any, Iterator
+from .container import ContainerInterface
+
+class Array(ContainerInterface):
+    """
+    ### An implementation of an array.  
+    - An array represents a collection of elements, with a predefined size.
+    """
+    def __init__(self, size: int) -> None:
+        self.items: list[Any] = []
+        for _ in range(0, size):
+            self.items.append(None)
+    
+    def __iter__(self) -> Iterator[Any]:
+        return iter(self.items)
+    
+    def __getitem__(self, idx: int) -> Any:
+        if idx >= len(self.items) or idx < 0:
+            raise IndexError("Index out of range")
+        else:
+            return self.items[idx]
+    
+    def __setitem__(self, idx: int, value: Any) -> None:
+        if idx >= len(self.items) or idx < 0:
+            raise IndexError("Index out of range")
+        else:
+            self.items[idx] = value
+    
+    def __len__(self) -> int:
+        return len(self.items)
---
+++ tests/test_array.py
@@ -0,0 +1,65 @@
+from src._array import Array
+import pytest
+
+def test_initialization():
+    array = Array(5)
+    assert len(array) == 5
+    for i in range(5):
+        assert array[i] is None
+    
+    assert type(array) == Array
+
+def test_getitem_setitem():
+    array = Array(5)
+    array[0] = 1
+    array[4] = 5
+    assert array[0] == 1
+    assert array[4] == 5
+    assert type(array) == Array
+
+def test_getitem_invalid_index():
+    array = Array(5)
+    with pytest.raises(IndexError):
+        _ = array[5]
+    with pytest.raises(IndexError):
+        _ = array[-1]
+    
+    assert type(array) == Array
+
+def test_setitem_invalid_index():
+    array = Array(5)
+    with pytest.raises(IndexError):
+        array[5] = 1
+    with pytest.raises(IndexError):
+        array[-1] = 1
+    
+    assert type(array) == Array
+
+def test_len():
+    array = Array(10)
+    assert len(array) == 10
+
+def test_iter():
+    array = Array(5)
+    for i in range(5):
+        array[i] = i
+    
+    for i, item in enumerate(array):
+        assert item == i
+    
+    assert type(array) == Array
+
+def test_datatypes():
+    array = Array(5)
+    array[0] = 1
+    array[1] = 2.5
+    array[2] = "three"
+    array[3] = [4, 5]
+    array[4] = {"six": 6}
+
+    assert array[0] == 1
+    assert array[1] == 2.5
+    assert array[2] == "three"
+    assert array[3] == [4, 5]
+    assert array[4] == {"six": 6}
+    assert type(array) == Array
